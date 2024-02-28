fbfd587c9beb55eeb09ad69e75b6541de3016917
LijinWilson <lijinwilson2018@gmail.com>
2024-02-28 12:50:12 +0530

    re commit the hdl bits qn-2

---
+++ hdl_bits_Qn-2.v
@@ -0,0 +1,10 @@
+// Problem statement
+// Build a circuit with no inputs and one output that outputs a constant 0
+
+// Solution: 
+
+module top_module(
+	output zero
+);
+assign zero = 0;
+endmodule
