ea412eb3d4b5106358c841439f84b0cbc19d22ae
LijinWilson <lijinwilson2018@gmail.com>
2024-02-28 12:46:41 +0530

    hdl bits 3rd question solution

---
+++ hdl_bits_Qn-3.v
@@ -0,0 +1,7 @@
+// Problem statement
+// create a wire (in green) by adding an assign statement to connect in to out
+
+// Solution: 
+module top_module( input in, output out );
+	assign out = in;
+endmodule
