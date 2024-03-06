393c73cc9516941a08bb9e092cf071b7fe4f94fc
LijinWilson <lijinwilson2018@gmail.com>
2024-03-06 22:32:02 +0530

    Commit for adder 1 in modules

--- Verilog Language/Modules/adder_1.v
+++ Verilog Language/Modules/adder_1.v
@@ -13,5 +13,6 @@ module top_module(
     wire add1_carry;
     // module add16 ( input[15:0] a, input[15:0] b, input cin, output[15:0] sum, output cout );
     add16 add1(.a(a[15:0]),.b(b[15:0]),.sum(sum[15:0]),.cout(add1_carry));
-    add16 add2(.a(a[32:16]),.b(b[32:16]),.sum(sum[32:16],.cout(add1_carry));
+    add16 add2(.a(a[31:16]),.b(b[31:16]),.sum(sum[31:16]),.cin(add1_carry));
 endmodule
+
