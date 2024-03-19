521dd8a0a92f283d119af9cdf33d2046a23c9a88
LijinWilson <lijinwilson2018@gmail.com>
2024-03-19 16:11:05 +0530

    Commit for More logic gate and truth table solution using k map reduction

--- Verilog Language/6 - Combinational Logic/1 - Basic Gate/8 - Truth_tables.v
+++ Verilog Language/6 - Combinational Logic/1 - Basic Gate/8 - Truth_tables.v
@@ -21,5 +21,31 @@ module top_module(
     and(and4_out, x3, x2, x1);
     
     or(f, and1_out, and2_out, and3_out, and4_out);
+
     
+endmodule
+
+// ------ Using K-Map Reduction --------
+
+module top_module( 
+    input x3,
+    input x2,
+    input x1,  // three inputs
+    output f   // one output
+);
+    
+    // Solved using the K-Map
+    // While solving the K-Map we are considering the sop ie; we consirdering the O/P as 1 term from the TT;
+    // After the K-Map reduction we get ~X3X2 + X3X1;
+    
+    // Declaring the wire for getting the output from the AND gate;
+    wire and1_out, and2_out;
+    
+    // AND operation
+    and(and1_out, ~x3, x2);
+    and(and2_out, x3, x1);
+    
+    // Summing the output from the and - SOP operation
+    or(f, and1_out, and2_out);
+
 endmodule
